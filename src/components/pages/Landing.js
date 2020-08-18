95a5e6ad79b304e3e074e1b41754d4917cd15617
Alangsam <alex@Alexs-MacBook-Pro.local>
2020-08-18 11:39:38 -0700

    .

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -2,22 +2,27 @@ import React from "react";
 import axios from "axios";
 
 export default class Landing extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      customers: [],
-    };
-  }
+    constructor() {
+        super();
+        this.state = {
+            customers: [],
+        };
+    }
 
-  componentDidMount() {
-    // axios.get()
-  }
+    componentDidMount() {
+        axios
+            .get(
+                "https://raw.githubusercontent.com/Alangsam/solstice/master/initial.js"
+            )
+            .then((res) => console.log(res.data))
+            .catch((err) => console.log(err));
+    }
 
-  render() {
-    return (
-      <div>
-        <table>{}</table>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <table>{}</table>
+            </div>
+        );
+    }
 }
